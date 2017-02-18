6b5fc5d1657dbad6520ef7abb9794c0ee26e41f9
blah blah <sa-mm@users.noreply.github.com>
2017-02-17 20:20:48 -0600

    fix typo

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.get('/*', function (req, res) {
 })
 
 app.listen(app.get('port'), function() {
-  console.log('Timestamp microservice listening on port' + app.get('port'))
+  console.log('Timestamp microservice listening on port ' + app.get('port'))
 })
